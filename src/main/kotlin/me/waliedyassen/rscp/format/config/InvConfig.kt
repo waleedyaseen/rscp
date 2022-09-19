669e06370a309b04bf64d7f975cd763205ae2970
Walied K. Yassen <walied.developer@hotmail.com>
2022-09-19 05:18:05 +0300

    Add stackall property to InvConfig

--- src/main/kotlin/me/waliedyassen/rscp/format/config/InvConfig.kt
+++ src/main/kotlin/me/waliedyassen/rscp/format/config/InvConfig.kt
@@ -22,11 +22,13 @@ class InvConfig(name: String) : Config(name, SymbolType.Inv) {
 
     var size = 0
     var scope = InvScope.TEMPORARY
+    var stackAll = false
 
     override fun parseProperty(name: String, parser: Parser) {
         when (name) {
             "size" -> size = parser.parseInteger() ?: return parser.skipProperty()
             "scope" -> scope = parser.parseEnumLiteral() ?: return parser.skipProperty()
+            "stackall" -> stackAll = parser.parseBoolean()
             "transmit" -> transmit = parser.parseBoolean()
             else -> parser.unknownProperty()
         }
@@ -42,18 +44,25 @@ class InvConfig(name: String) : Config(name, SymbolType.Inv) {
 
     override fun encode(side: Side, sym: SymbolTable): ByteArray {
         val packet = BinaryEncoder(1 + if (size != 0) 3 else 0)
-        if (side == Side.Server || transmit) {
-            if (side == Side.Server) {
-                if (scope != InvScope.TEMPORARY) {
-                    packet.code(1) {
-                        write1(scope.id)
-                    }
+        if (side != Side.Server && !transmit) {
+            packet.terminateCode()
+            return packet.toByteArray()
+        }
+        if (side == Side.Server) {
+            if (scope != InvScope.TEMPORARY) {
+                packet.code(1) {
+                    write1(scope.id)
                 }
             }
-            if (size != 0) {
-                packet.code(2) {
-                    write2(size)
+            if (stackAll) {
+                packet.code(3) {
+                    write1(1)
                 }
+            }
+        }
+        if (size != 0) {
+            packet.code(2) {
+                write2(size)
             }
         }
         packet.terminateCode()
