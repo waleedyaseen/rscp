5453edb66c08274eaf0da39fe07bdd3577281c8f
Walied K. Yassen <walied.developer@hotmail.com>
2023-03-12 00:26:42 +0300

    Add SymbolType#id for lookup by id

--- src/main/kotlin/me/waliedyassen/rscp/symbol/SymbolType.kt
+++ src/main/kotlin/me/waliedyassen/rscp/symbol/SymbolType.kt
@@ -15,13 +15,14 @@ import me.waliedyassen.rscp.format.iftype.Component
 /**
  * A primitive symbol type that cannot be stored in a symbol table.
  */
-open class PrimitiveSymbolType(legacyChar: kotlin.Char, literal: kotlin.String) :
-    SymbolType<BasicSymbol>(legacyChar, literal, serializer = BasicSymbolSerializer)
+open class PrimitiveSymbolType(id: kotlin.Int, legacyChar: kotlin.Char, literal: kotlin.String) :
+    SymbolType<BasicSymbol>(id, legacyChar, literal, serializer = BasicSymbolSerializer)
 
 /**
  * The base symbol type.
  */
 open class SymbolType<T : Symbol>(
+    val id: kotlin.Int,
     val legacyChar: kotlin.Char,
     val literal: kotlin.String,
     val constructor: ((kotlin.String) -> Config)? = null,
@@ -57,50 +58,51 @@ open class SymbolType<T : Symbol>(
         else -> false
     }
 
-    object Undefined : PrimitiveSymbolType(0.toChar(), "")
+    object Undefined : PrimitiveSymbolType(-1, 0.toChar(), "")
     object ClientScript :
-        SymbolType<ClientScriptSymbol>(0.toChar(), "clientscript", null, ClientScriptSymbolSerializer)
+        SymbolType<ClientScriptSymbol>(-1, 0.toChar(), "clientscript", null, ClientScriptSymbolSerializer)
 
-    object Int : PrimitiveSymbolType('i', "int")
-    object Boolean : PrimitiveSymbolType('1', "boolean")
-    object Seq : PrimitiveSymbolType('A', "seq")
-    object LocShape : PrimitiveSymbolType('H', "locshape")
-    object Component : SymbolType<BasicSymbol>('I', "component", ::Component, BasicSymbolSerializer, extension = null)
-    object NamedObj : PrimitiveSymbolType('O', "namedobj")
-    object Synth : PrimitiveSymbolType('P', "synth")
-    object Area : PrimitiveSymbolType('R', "area")
-    object Stat : PrimitiveSymbolType('S', "stat")
-    object NpcStat : PrimitiveSymbolType('T', "npc_stat")
-    object MapArea : PrimitiveSymbolType('`', "wma")
-    object CoordGrid : PrimitiveSymbolType('c', "coord")
-    object Graphic : SymbolType<BasicSymbol>('d', "graphic", ::GraphicConfig, BasicSymbolSerializer)
-    object FontMetrics : PrimitiveSymbolType('f', "fontmetrics")
-    object Enum : SymbolType<TypedSymbol>('g', "enum", ::EnumConfig, TypedSymbolSerializer)
-    object Loc : SymbolType<BasicSymbol>('l', "loc", ::LocConfig, BasicSymbolSerializer)
-    object Model : PrimitiveSymbolType('m', "model")
-    object Npc : PrimitiveSymbolType('n', "npc")
-    object Obj : PrimitiveSymbolType('o', "obj")
-    object String : PrimitiveSymbolType('s', "string")
-    object Spotanim : PrimitiveSymbolType('t', "spotanim")
-    object NpcUid : PrimitiveSymbolType('u', "npc_uid")
-    object Inv : SymbolType<BasicSymbol>('v', "inv", ::InvConfig, BasicSymbolSerializer)
-    object Texture : PrimitiveSymbolType('x', "texture")
-    object Category : PrimitiveSymbolType('y', "category")
-    object Char : PrimitiveSymbolType('z', "char")
-    object MapElement : PrimitiveSymbolType('µ', "mapelement")
-    object Interface : PrimitiveSymbolType('a', "interface")
-    object TopLevelInterface : PrimitiveSymbolType('F', "toplevelinterface")
-    object OverlayInterface : PrimitiveSymbolType('L', "overlayinterface")
-    object ClientInterface : PrimitiveSymbolType('©', "clientinterface")
-    object NewVar : PrimitiveSymbolType('-', "newvar")
-    object VarPlayer : SymbolType<TypedSymbol>('\u0000', "varp", ::VarpConfig, TypedSymbolSerializer)
-    object VarClient : SymbolType<TypedSymbol>('\u0000', "varc", ::VarcConfig, TypedSymbolSerializer)
-    object VarBit : SymbolType<BasicSymbol>('\u0000', "varbit", ::VarbitConfig, BasicSymbolSerializer)
-    object Param : SymbolType<ConfigSymbol>('\u0000', "param", ::ParamConfig, ConfigSymbolSerializer)
-    object Struct : SymbolType<BasicSymbol>('J', "struct", ::StructConfig, BasicSymbolSerializer)
-    object DbRow : PrimitiveSymbolType('Ð', "dbrow")
-    object Constant : SymbolType<ConstantSymbol>('^', "constant", null, ConstantSymbolSerializer)
-    object AutoInt : PrimitiveSymbolType('\u0000', "autoint")
+    object Int : PrimitiveSymbolType(0, 'i', "int")
+    object Boolean : PrimitiveSymbolType(1, '1', "boolean")
+    object Seq : PrimitiveSymbolType(6, 'A', "seq")
+    object LocShape : PrimitiveSymbolType(8, 'H', "locshape")
+    object Component :
+        SymbolType<BasicSymbol>(9, 'I', "component", ::Component, BasicSymbolSerializer, extension = null)
+
+    object NamedObj : PrimitiveSymbolType(13, 'O', "namedobj")
+    object Synth : PrimitiveSymbolType(14, 'P', "synth")
+    object Area : PrimitiveSymbolType(16, 'R', "area")
+    object Stat : PrimitiveSymbolType(17, 'S', "stat")
+    object NpcStat : PrimitiveSymbolType(18, 'T', "npc_stat")
+    object MapArea : PrimitiveSymbolType(21, '`', "wma")
+    object CoordGrid : PrimitiveSymbolType(22, 'c', "coord")
+    object Graphic : SymbolType<BasicSymbol>(23, 'd', "graphic", ::GraphicConfig, BasicSymbolSerializer)
+    object FontMetrics : PrimitiveSymbolType(25, 'f', "fontmetrics")
+    object Enum : SymbolType<TypedSymbol>(26, 'g', "enum", ::EnumConfig, TypedSymbolSerializer)
+    object Loc : SymbolType<BasicSymbol>(30, 'l', "loc", ::LocConfig, BasicSymbolSerializer)
+    object Model : PrimitiveSymbolType(31, 'm', "model")
+    object Npc : PrimitiveSymbolType(32, 'n', "npc")
+    object Obj : PrimitiveSymbolType(33, 'o', "obj")
+    object String : PrimitiveSymbolType(36, 's', "string")
+    object Spotanim : PrimitiveSymbolType(37, 't', "spotanim")
+    object NpcUid : PrimitiveSymbolType(38, 'u', "npc_uid")
+    object Inv : SymbolType<BasicSymbol>(39, 'v', "inv", ::InvConfig, BasicSymbolSerializer)
+    object Texture : PrimitiveSymbolType(40, 'x', "texture")
+    object Category : PrimitiveSymbolType(41, 'y', "category")
+    object Char : PrimitiveSymbolType(42, 'z', "char")
+    object MapElement : PrimitiveSymbolType(59, 'µ', "mapelement")
+    object Interface : PrimitiveSymbolType(97, 'a', "interface")
+    object TopLevelInterface : PrimitiveSymbolType(98, 'F', "toplevelinterface")
+    object OverlayInterface : PrimitiveSymbolType(99, 'L', "overlayinterface")
+    object ClientInterface : PrimitiveSymbolType(100, '©', "clientinterface")
+    object NewVar : PrimitiveSymbolType(-1, '-', "newvar")
+    object VarPlayer : SymbolType<TypedSymbol>(-1, '\u0000', "varp", ::VarpConfig, TypedSymbolSerializer)
+    object VarClient : SymbolType<TypedSymbol>(-1, '\u0000', "varc", ::VarcConfig, TypedSymbolSerializer)
+    object VarBit : SymbolType<BasicSymbol>(-1, '\u0000', "varbit", ::VarbitConfig, BasicSymbolSerializer)
+    object Param : SymbolType<ConfigSymbol>(-1, '\u0000', "param", ::ParamConfig, ConfigSymbolSerializer)
+    object Struct : SymbolType<BasicSymbol>(73, 'J', "struct", ::StructConfig, BasicSymbolSerializer)
+    object Constant : SymbolType<ConstantSymbol>(-1, '\u0000', "constant", null, ConstantSymbolSerializer)
+    object AutoInt : PrimitiveSymbolType(-1, '\u0000', "autoint")
 
 
     companion object {
@@ -147,7 +149,6 @@ open class SymbolType<T : Symbol>(
             VarBit,
             Param,
             Struct,
-            DbRow,
             Constant,
         )
 
@@ -157,6 +158,7 @@ open class SymbolType<T : Symbol>(
         private val lookupByLiteral = values
             .filter { it.literal.isNotBlank() }
             .associateBy { it.literal }
+
         /**
          * A look-up by literal map for [SymbolType].
          */
